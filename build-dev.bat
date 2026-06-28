9556eef4d6de17f10a6beff473180cdd59775552
xuchaoji <admin@xuchaoji.com>
2026-06-28 16:03:24 +0800

    增加服务自启动功能

--- build-dev.bat
+++ build-dev.bat
@@ -1,2 +1,3 @@
 hvigorw --mode module -p module=main@dev -p product=dev -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=normal --parallel --incremental --daemon
+if %ERRORLEVEL% EQU 0 hdc install "main\build\dev\outputs\dev\main-dev-signed.hap"
 @pause
